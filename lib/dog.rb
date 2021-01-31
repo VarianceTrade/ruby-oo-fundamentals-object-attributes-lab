2a013c48487aa7d36128ac51b4163a0caeeec906
Brandon <sugb73@gmail.com>
2021-01-30 20:42:41 -0500

    commit tree 74aa1f2adaa82cce17fcc5f5c7c886bf494f9955

---
+++ lib/dog.rb
@@ -0,0 +1,34 @@
+class Dog
+    def initialize()
+    end
+
+    def name()
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(name)
+        @name = name
+    end
+    
+    def breed=(breed)
+        @breed = breed
+    end
+end
+
+# my_dog = Dog.new("Sam", "Lab")
+
+# my_dog_2 = Dog.new("Fido", "Poodle")
+
+# my_dog.name = "Fido"
+# my_dog.name = "Sam"
+
+# my_dog.breed = "Pit bull"
+
+# my_dog = Dog.new
+
+# my_dog.name = "Fido"
+# my_dog.breed = 
